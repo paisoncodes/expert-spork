ca315eda516b7c30a2c3cf34ced48d82f08fe362
Olatunji Komolafe <iamkomolafe.o.s@gmail.com>
2023-03-07 18:46:36 +0100

    added command to create default role

--- role/management/commands/create_default_role.py
+++ role/management/commands/create_default_role.py
@@ -6,11 +6,12 @@ from role.models import Role, RolePermission
 class Command(BaseCommand):
 
     def add_default_role_and_permission(self) -> None:
+        permission, created = RolePermission.objects.get_or_create(name="Add Subscription", method="POST", module_name="Role")
         if role := Role.objects.filter(name__iexact="Default Role").first():
-            role.permissions.add(RolePermission.objects.create(name="Add Subscription", method="POST", module_name="Role"))
+            role.permissions.add(permission)
         else:
             role = Role.objects.create(name="Default Role")
-            role.permissions.add(RolePermission.objects.create(name="Add Subscription", method="POST", module_name="Role"))
+            role.permissions.add(permission)
 
     def handle(self, *args, **options):
         print("Started")
